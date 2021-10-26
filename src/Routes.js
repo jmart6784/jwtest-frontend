e83c174e95aea86bd60c2db62b536ca0ea38690c
jmart6784 <jmartinez6784@yahoo.com>
2021-10-25 17:00:57 -0700

    Created sign up page and added it to routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
+import SignUp from "./components/users/SignUp";
 
 const Routes = () => {
   return (
     <HashRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exatc path="/sign_up" component={SignUp} />
       </Switch>
     </HashRouter>
   );
---
+++ src/components/users/SignUp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <h1>Sign In</h1>;
+};
+
+export default SignUp;

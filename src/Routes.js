ea926edf3768d60b5b1e45d276c9d4a8d267b8da
jmart6784 <jmartinez6784@yahoo.com>
2021-10-26 22:30:46 -0700

    Created global context

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import GlobalContext from "./components/context/GlobalContext";
 import SignUp from "./components/users/SignUp";
 import SignIn from "./components/users/SignIn";
 import NoteIndex from "./components/notes/NoteIndex";
@@ -7,21 +8,24 @@ import Nav from "./components/layouts/Nav";
 import Static from "./components/static";
 
 const Routes = () => {
+  const [globalContext, setGlobalContext] = useState({});
   let loggedIn = !!localStorage.getItem("token");
 
   return (
-    <HashRouter>
-      <Nav />
-      <Switch>
-        <Route exact path="/">
-          {loggedIn ? <Redirect to="/notes" /> : <Redirect to="/sign_in" />}
-        </Route>
-        <Route exact path="/notes" component={NoteIndex} />
-        <Route exact path="/static" component={Static} />
-        <Route exact path="/sign_up" component={SignUp} />
-        <Route exact path="/sign_in" component={SignIn} />
-      </Switch>
-    </HashRouter>
+    <GlobalContext.Provider value={[globalContext, setGlobalContext]}>
+      <HashRouter>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            {loggedIn ? <Redirect to="/notes" /> : <Redirect to="/sign_in" />}
+          </Route>
+          <Route exact path="/notes" component={NoteIndex} />
+          <Route exact path="/static" component={Static} />
+          <Route exact path="/sign_up" component={SignUp} />
+          <Route exact path="/sign_in" component={SignIn} />
+        </Switch>
+      </HashRouter>
+    </GlobalContext.Provider>
   );
 };
 

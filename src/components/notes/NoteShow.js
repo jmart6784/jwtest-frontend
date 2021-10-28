28d3aba3945270921ed167463dd4c06929e6899b
jmart6784 <jmartinez6784@yahoo.com>
2021-10-27 20:45:32 -0700

    Added note show logic

--- src/components/notes/NoteShow.js
+++ src/components/notes/NoteShow.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import GlobalContext from "../context/GlobalContext";
 
-const NoteShow = () => {
+const NoteShow = (props) => {
+  // eslint-disable-next-line no-unused-vars
+  const [globalContext, setGlobalContext] = useContext(GlobalContext);
+  const [note, setNote] = useState({ message: "" });
+
+  useEffect(() => {
+    if (localStorage.getItem("token") === null) props.history.push("/sign_in");
+
+    const url = `${globalContext.domain}/notes/${props.match.params.id}`;
+
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setNote(response))
+      .catch(() => props.history.push("/"));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <h1>Note Show</h1>
+      <p>Message {note.message}</p>
     </div>
   );
 };

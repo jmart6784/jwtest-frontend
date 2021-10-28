ecf5349d3bb761a609d5b7df316d6b3e47aafe13
jmart6784 <jmartinez6784@yahoo.com>
2021-10-28 00:37:05 -0700

    added user show request data to front end

--- src/components/users/UserShow.js
+++ src/components/users/UserShow.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+import GlobalContext from "../context/GlobalContext";
 
-const UserShow = () => {
+const UserShow = (props) => {
+  const [globalContext, setGlobalContext] = useContext(GlobalContext);
+  const [user, setUser] = useState({
+    id: "",
+    username: "",
+    age: "",
+    created_at: "",
+    updated_at: "",
+  });
+
+  useEffect(() => {
+    if (localStorage.getItem("token") === null) props.history.push("/sign_in");
+
+    const url = `${globalContext.domain}/users/${props.match.params.id}`;
+
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setUser(response))
+      .catch(() => props.history.push("/"));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <h1>User Show</h1>
+      <p>Username: {user.username}</p>
+      <p>age: {user.age}</p>
     </div>
   );
 };
